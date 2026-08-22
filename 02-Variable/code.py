d1031ee48ac981dd679296b021fab51f4e14f854
Praveen-kumar-cg <praveen.bishnoi.cg@gmail.com>
2026-08-22 16:16:55 +0530

    Variable assignment publish

---
+++ 02-Variable/code.py
@@ -0,0 +1,32 @@
+#Q=24
+student_name="Praveen kumar"
+student_age=18
+student_city="Jalore"
+
+#Q=25
+student_name="Praveen kumar"
+roll_number=112669
+student_branch="cse"
+
+#Q=26
+marks=200
+marks=2000
+
+#Q=27
+name,age,city="praveen kumar",18,"jalore"
+
+#Q=28
+x=y=z=0
+
+#Q=29
+student1="rahul"
+student_name="Rahul"
+Class="btech"
+
+
+
+#Q=30
+student_name="Praveen kumar"
+stuudent_age=18
+student_age=19
+print(student_name,student_age)
