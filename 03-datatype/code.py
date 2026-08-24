4ad3b27659632ff15b705aa4c6a28c0c347997be
Praveen-kumar-cg <praveen.bishnoi.cg@gmail.com>
2026-08-24 21:20:21 +0530

    datatype assignment published

---
+++ 03-datatype/code.py
@@ -0,0 +1,48 @@
+#Q=24
+student_name="Praveen kumar"
+student_age=18
+student_height=5.8
+is_student=True
+result=None
+print("output of question 24:",type(student_name),type(student_age),type(student_height),type(is_student),type(result))
+
+#Q=25
+a=50
+b=50.0
+c="50"
+print("output of question 25:",type(a),type(b),type(c))
+
+#Q=26
+a=True
+b="True"
+print("output of question 26:",type(a),type(b))
+
+#Q=27
+a=None
+b="None"
+print("output of question 27:",type(a),type(b))
+
+#Q=28
+value=18
+print("output of question 28:",type(value))
+value=str(value)
+print("output of question 28:",type(value))
+
+#Q=29
+product_name="laptop"
+product_quentity=200
+product_price=199999.0
+is_available=True
+discount=None
+print("output of question 29:",type(product_name),type(product_quentity),type(product_price),type(is_available),type(discount))
+
+
+#Q=30
+a=10
+b=10.0
+c="10"
+d=True
+e="True"
+f=None
+g="None"
+print("output of question 30:",type(a),type(b),type(c),type(d),type(e),type(f),type(g))
