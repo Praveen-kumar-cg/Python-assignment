f42b66bcab1280aa18c99ceab116ab2db5da433f
Praveen-kumar-cg <praveen.bishnoi.cg@gmail.com>
2026-08-26 17:38:06 +0530

    new file added

---
+++ 05-String/code.py
@@ -0,0 +1,13 @@
+#Part 3 — String Creation and Basic Operations
+#Task 1 — Create Strings
+your_name="Praveen"
+your_city='Sanchore'
+favorite_language='Python'
+massage="my name is praveen kumar \n form sanchore rajasthan and \n i am btech cse student at coding gita"
+print("Output for task-1:-")
+print(your_city,your_name,favorite_language,massage)
+
+#Task 2 — Empty String
+name=""
+print("Output for task-2:-")
+print(name,"length of string=",len(name),type(name))
