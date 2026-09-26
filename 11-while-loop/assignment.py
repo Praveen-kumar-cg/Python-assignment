bff38ee1a123679d0092d49b040bce869ad4f133
Praveen-kumar-cg <praveen.bishnoi.cg@gmail.com>
2026-09-26 22:31:17 +0530

    while loop assignment1 published

---
+++ 11-while-loop/assignment.py
@@ -0,0 +1,254 @@
+# #Q1
+# i=1
+# while i<=5:
+#     print("Hello")
+#     i+=1
+
+
+# #Q2
+# i=0
+# while i<10:
+#     print(i,end=" ")
+#     i+=1
+
+
+# #Q3
+# i=1
+# while i<=10:
+#     print(i)
+#     i+=1
+
+# #Q4
+# i=10
+# while i>=1:
+#     print(i)
+#     i-=1
+
+# #Q5
+# i=5
+# while i<=50:
+#     print(i)
+#     i+=5
+
+# #Q6
+# i=2
+# while i<=20:
+#     print(i)
+#     i+=2
+
+
+# #Q7
+# i=1
+# while i<20:
+#     print(i)
+#     i+=1
+
+
+# #Q8
+# i=3
+# while i<20:
+#     print(i)
+#     i+=3
+
+# #Q9
+# i=20
+# while i>1:
+#     print(i)
+#     i-=2
+
+# #Q10
+# n=int(input("enter an numbre:-"))
+# i=1
+# while i<=n:
+#     print(i)
+#     i+=1
+
+# #Q11
+# n=int(input("enter an number:-"))
+# i=2
+# while i<n:
+#     print(i)
+#     i+=2
+
+# #Q12
+# n=int(input("enter an number:-"))
+# i=1
+# while i<n:
+#     print(i)
+#     i+=2
+
+
+# #Q13
+# n=int(input("enter an number:-"))
+# i=1
+# while i<=n:
+#     if i%3==0:
+#         print(i)
+#     i+=1
+
+# #Q14
+# n=int(input("enter an number:-"))
+# i=1
+# while i<=n:
+#     if i%3==0 and i%2==0:
+#         print(i)
+#     i+=1
+
+
+# #Q15
+# n=int(input("enter an number:-"))
+# i=1
+# even_count=0
+# while i<=n:
+#     if i%2==0:
+        
+#         even_count+=1
+#     i+=1
+# print("the total number of even count is ",even_count)
+
+
+# #Q16
+# n=int(input("enter an number:-"))
+# total=0
+# i=1
+# while i<=n:
+#     total+=i
+#     i+=1
+# print("the total is ",total)
+
+#Q17
+# n=int(input("enter an number:-"))
+# i=2
+# total=0
+# while i<=n:
+#     total+=i
+#     i+=2
+# print("the total of even number is ",total)
+
+
+#Q18
+# n=int(input("enter an number:-"))
+# i=1
+# total=0
+# while i<=n:
+#     total+=i
+#     i+=2
+# print("the total of even number is ",total)
+
+# #Q19
+# n=int(input("enter an number:-"))
+# i=1
+# while i<=10:
+#     print(n,"X",i,"=>",n*i)
+#     i+=1
+
+#Q20
+# n=int(input("enter an number:-"))
+# i=1
+# total=1
+# while i<=n:
+#     total*=i
+#     i+=1
+# print("the total is ",total)
+
+
+# #Q21
+# str=input("enter an string:-")
+# i=0
+# while i<len(str):
+#     print(str[i])
+#     i+=1 
+
+# #Q22
+# str=input("enter an string:-")
+# i=0
+# while i<len(str):
+#     print(str[i],end=" ")
+#     i+=1
+
+# #Q23
+# str=input("enter an string:-")
+# i=0
+# length=0
+# while i<len(str):
+#     length+=1
+#     i+=1
+# print("the length of string is ",length)
+
+# #Q24
+# str=input("enter an string:-")
+# i=0
+# count=0
+# while i<len(str):
+#     if str[i]=="a":
+#         count+=1
+#     i+=1
+# print("the frequency of a is",count)
+
+# #Q25
+# str=input("enter an string:-")
+# i=0
+# upper_count=0
+# while i<len(str):
+#     if "A"<=str[i]<="Z":
+#         upper_count+=1
+#     i+=1
+# print("the frequency of Uppercase is",upper_count)
+
+# #Q26
+# n=int(input("enter an number:-"))
+# i=1
+# while i<=n:
+#     j=1
+#     while j<=n:
+#         print("*",end=" ")
+#         j+=1
+#     print()
+#     i+=1
+
+
+# #Q27
+# n=int(input("enter an number:-"))
+# i=1
+# while i<=n:
+#     j=1
+#     while j<=n:
+#         print("*",end=" ")
+#         j+=1
+#     print()
+#     i+=1
+
+# #Q28
+# n=int(input("enter an number:-"))
+# i=1
+# while i<=n:
+#     j=1
+#     while j<=i:
+#         print("*",end=" ")
+#         j+=1
+#     print()
+#     i+=1
+
+
+# #Q29
+# n=int(input("enter an number:-"))
+# i=1
+# while i<=n:
+#     j=1
+#     while j<=i:
+#         print(j,end=" ")
+#         j+=1
+#     print()
+#     i+=1
+
+
+# #Q30
+# n=int(input("enter an number:-"))
+# i=1
+# while i<=n:
+#     j=1
+#     while j<=10:
+#         print(i*j,end=" ")
+#         j+=1
+#     print()
+#     i+=1
